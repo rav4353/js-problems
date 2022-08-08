e0fdf28c166674c9325afd109a42701d20fd47a4
rav4353 <ravanthsri20@gmail.com>
2022-08-08 09:30:55 +0530

    hi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,109 @@
-const a=15;
-const b=20;
+//addition
 
-const sum=a+b;
+const num1 = 15;
+const num2 = 20;
 
-document.write('sum of ' + a + ' and ' + b + ' is: ' +sum);
+const add = num1 + num2;
+
+console.log('sum of ' + num1 + ' and ' + num2 + ' is: ' + add);
+
+
+
+//subraction
+
+
+const num3 = 15;
+const num4 = 20;
+
+const sub = num3 - num4;
+
+console.log('sum of ' + num3 + ' and ' + num4 + ' is: ' + sub);
+
+
+
+
+//multipication
+
+
+const num5 = 15;
+const num6 = 20;
+
+const mul = num5 * num6;
+
+console.log('sum of ' + num5 + ' and ' + num6 + ' is: ' + mul);
+
+
+
+//divide
+
+
+const num7 = 15;
+const num8 = 20;
+
+const div = num7 / num8;
+
+console.log('sum of ' + num7 + ' and ' + num8 + ' is: ' + div);
+
+
+
+//area of a circle
+
+var r = 20;
+
+
+console.log('The area of circle with radius 20cm is: ' + Math.PI * r * r);
+
+
+//area of a triangle
+
+const base = 10;
+const height = 15;
+const areaOfTriangle = (base * height) / 2;
+
+console.log('area of a triangle:' + areaOfTriangle);
+
+
+//simple interest
+
+
+var p, t, r, SI;
+
+p = 15;
+t = 12;
+r = 12;
+
+SI = (p * t * r) / 100;
+
+console.log("Simple Interest = " + SI);
+
+
+//area of square
+
+var areaOfSquare = 20;
+
+
+console.log('The area of square is: ' + areaOfSquare * areaOfSquare);
+
+
+
+//area of cube
+
+const a = 10;
+const areaOfCube = 6*(a*a);
+
+console.log('The area of Cube is: ' + areaOfCube);
+
+
+
+//Swap two numbers 
+
+let number1 = 5;
+let number2 = 10;
+
+let temp;
+
+temp = number1;
+number1 = number2;
+number2 = temp;
+
+console.log('The value of a after swapping: ' + number1 + ':' + number2);
