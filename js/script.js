b128998f5959c56211e0e8e65d8079dc2a12818d
rav4353 <ravanthsri20@gmail.com>
2022-08-21 19:46:06 +0530

    hi

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ console.log('area of a triangle:' + areaOfTriangle);
 
 var p, t, r, SI;
 
-p = 15;
+p = 15000;
 t = 12;
 r = 12;
 
@@ -103,7 +103,152 @@ let number2 = 10;
 let temp;
 
 temp = number1;
+console.log(number1);
 number1 = number2;
 number2 = temp;
 
 console.log('The value of a after swapping: ' + number1 + ':' + number2);
+
+// 11. Program to calculate total and average
+
+const arr = [10, 20, 30, 40, 50];
+let sum = 0;
+for (var number of arr) {
+    sum += number;
+}
+average = sum / arr.length;
+console.log(average);
+
+// 12. Program for large number
+
+function maxOfThree(x, y, z) 
+ {
+  maxVal = 0;
+  if (x > y)
+  {
+    maxVal = x;
+  } else
+  {
+    maxVal = y;
+  }
+  if (z > maxVal) 
+  {
+    maxVal = z;
+  }
+  return maxVal;
+}
+
+console.log(maxOfThree(1000,510,440));
+
+// 13. Program to check number is positive or not
+
+let number11 = prompt(" Q: Program to check number is positive or not \n Input: Enter the number", "");
+number11 = parseInt(number11);
+
+if(number11 < 0)
+    console.log("\nYou entered Negative Number.");
+else if(number11 > 0)
+    console.log("\nYou entered Positive Number.");
+else
+    console.log("\nYou entered Zero.");
+
+
+// 14. Program to check number is greater than 10
+
+let number21 = 10;
+
+if (number21 > 10) {
+  console.log('\nGiven number is greater than 10.');
+}
+
+// 15. Even or Odd
+
+const evenOdd = 17;
+
+if(evenOdd% 2 == 0) {
+    console.log("\nThe number is even.");
+}
+else {
+    console.log("\nThe number is odd.");
+}
+
+// 16. Program to check whether number is divisible by 5
+
+const divBy5 = 9;
+
+if (divBy5 % 2 === 0) {
+  console.log("Even")
+} else {
+ console.log("Odd") 
+}
+
+
+// 17. Program to check whether number is multiple of 7
+
+var multiOf7 = 12;
+
+if (multiOf7 % 3 == 0)
+	console.log('Multiple of 7')
+else 
+  console.log('Not a multiple of 7')
+
+// 18. Ascending and Decending order of 3 numbers
+
+// let = ascDes;
+// let ascDes = [5, 13, 1, 44, 32, 15, 500];
+
+// let ascending = aseDes.sort((a, b) => a - b);
+// let descending = aseDes.sort((a, b) => b - a);
+
+// console.log(ascending);
+// console.log(descending);
+
+// 21. Program to find marriage eligibility
+
+function validate(age) {
+var ans="not eigible";
+if(age >= 18){
+  ans = "eligible";
+}
+console.log("You are "+ans+" for marriage");
+}
+
+var age = 55;
+
+
+
+var year = 2000;
+if(year%4==0) {
+	console.log("leap year");
+}
+else {
+	console.log("non leap year");
+}
+
+var num = 2;
+if(num % 11==0)  {
+	console.log("divide by 11");
+	
+}
+else{
+	console.log("doesnt divide with 11");
+}
+
+
+var mark = 30;
+if(mark > 80) {
+	console.log("a grade");
+}
+else if (50< mark) {
+	console.log("b grade");
+}
+else if (35<mark){
+	console.log("c grade");
+}
+else if (35<=mark) {
+	console.log("d grade");
+}
+else {
+	console.log("fail");
+}
+	
